08e25f7efdbe999235eabc2f4814e5d048b6a699
苏祥 <60846352+MiracleStep@users.noreply.github.com>
2021-09-04 19:21:15 +0800

    调用链路追踪&服务熔断&访问限流

--- wanxinp2p-gateway-server/src/main/java/cn/itcast/wanxinp2p/gateway/config/ZuulConfig.java
+++ wanxinp2p-gateway-server/src/main/java/cn/itcast/wanxinp2p/gateway/config/ZuulConfig.java
@@ -1,5 +1,14 @@
 package cn.itcast.wanxinp2p.gateway.config;
 
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayFlowRule;
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayRuleManager;
+import com.alibaba.csp.sentinel.adapter.gateway.zuul.filters.SentinelZuulErrorFilter;
+import com.alibaba.csp.sentinel.adapter.gateway.zuul.filters.SentinelZuulPostFilter;
+import com.alibaba.csp.sentinel.adapter.gateway.zuul.filters.SentinelZuulPreFilter;
+import com.alibaba.csp.sentinel.datasource.apollo.ApolloDataSource;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
+import com.netflix.zuul.ZuulFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,6 +16,9 @@ import org.springframework.core.Ordered;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+
+import javax.annotation.PostConstruct;
+import java.util.Set;
 
 @Configuration
 public class ZuulConfig {
@@ -27,5 +39,37 @@ public class ZuulConfig {
         return bean;
     }
 
+    @Bean //pre过滤器，在请求路由之前进行限流操作
+    public ZuulFilter sentinelZuulPreFilter() {
+        return new SentinelZuulPreFilter();
+    }
+    @Bean //post过滤器，路由之后恢复资源
+    public ZuulFilter sentinelZuulPostFilter() {
+        return new SentinelZuulPostFilter();
+    }
+    @Bean //error过滤器，异常后的处理
+    public ZuulFilter sentinelZuulErrorFilter() {
+        return new SentinelZuulErrorFilter();
+    }
 
-}
+    /**
+    * 从Apollo上获取限流规则并进行配置
+    */
+    @PostConstruct
+    private void initGatewayRules() {
+        String namespaceName = "application"; // 对应Apollo的命名空间名称
+        String ruleKey = "sentinel.rule.gateway-flow-rule"; //限流规则的key
+        String defaultRules = "";
+
+        //从Apollo上获取限流规则并封装到ApolloDataSource
+        ApolloDataSource gatewayRuleDataSource = new ApolloDataSource<>(namespaceName,ruleKey,defaultRules,source->
+                        JSON.parseObject(source,new TypeReference<Set<GatewayFlowRule>>() {}));
+        //注册限流规则使其生效
+        GatewayRuleManager.register2Property(gatewayRuleDataSource.getProperty());
+
+    }
+
+
+
+
+    }
--- wanxinp2p-uaa-service/src/main/java/cn/itcast/wanxinp2p/uaa/agent/AccountApiAgent.java
+++ wanxinp2p-uaa-service/src/main/java/cn/itcast/wanxinp2p/uaa/agent/AccountApiAgent.java
@@ -2,14 +2,34 @@ package cn.itcast.wanxinp2p.uaa.agent;
 
 import cn.itcast.wanxinp2p.api.account.model.AccountDTO;
 import cn.itcast.wanxinp2p.api.account.model.AccountLoginDTO;
+import cn.itcast.wanxinp2p.common.domain.CommonErrorCode;
 import cn.itcast.wanxinp2p.common.domain.RestResponse;
+import feign.hystrix.FallbackFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(value = "account-service")
+@FeignClient(value = "account-service",fallbackFactory = AccountFallbackFactory.class)
 public interface AccountApiAgent {
 
     @PostMapping(value = "/account/l/accounts/session")
     RestResponse<AccountDTO> login(@RequestBody AccountLoginDTO accountLoginDTO);
+}
+
+@Component
+@Slf4j
+class AccountFallbackFactory implements FallbackFactory<AccountApiAgent>{
+
+    @Override
+    public AccountApiAgent create(Throwable cause) {
+        return new AccountApiAgent() {
+            @Override
+            public RestResponse<AccountDTO> login(AccountLoginDTO accountLoginDTO) {
+                log.info("fallback;reason was:",cause);
+                return new RestResponse<>(CommonErrorCode.E_999995.getCode(),CommonErrorCode.E_999995.getDesc());
+            }
+        };
+    }
 }
